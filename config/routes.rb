147d487211fe1bb5727d90d5c0dafe5366c56294
paul_massardo@hotmail.com <paul_massardo@hotmail.com>
2016-08-29 11:23:09 -0400

    changed default route to the user_session_new/login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
 
-  root :to => 'users#index'
+  # get 'password_resets/create'
+  #
+  # get 'password_resets/edit'
+  #
+  # get 'password_resets/update'
+
+
+
+  root :to => 'user_sessions#new'
   resources :user_sessions
   resources :users
+  resources :password_resets
 
   get 'login' => 'user_sessions#new', :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
