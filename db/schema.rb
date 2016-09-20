6fbf7a6973c3da98f90500c4b40160c356b6919a
paul_massardo@hotmail.com <paul_massardo@hotmail.com>
2016-09-20 00:35:47 -0400

    created task, room, room_task, and added notes to room_task

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160807160404) do
+ActiveRecord::Schema.define(version: 20160920025854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "room_tasks", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "task_id"
+    t.integer  "room_id"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text     "note"
+  end
+
+  add_index "room_tasks", ["room_id"], name: "index_room_tasks_on_room_id", using: :btree
+  add_index "room_tasks", ["task_id"], name: "index_room_tasks_on_task_id", using: :btree
+  add_index "room_tasks", ["user_id"], name: "index_room_tasks_on_user_id", using: :btree
+
+  create_table "rooms", force: :cascade do |t|
+    t.string   "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
-    t.string   "email",            null: false
+    t.string   "email",                           null: false
     t.string   "crypted_password"
     t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "reset_password_token"
+    t.datetime "reset_password_token_expires_at"
+    t.datetime "reset_password_email_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", using: :btree
 
+  add_foreign_key "room_tasks", "rooms"
+  add_foreign_key "room_tasks", "tasks"
+  add_foreign_key "room_tasks", "users"
 end
