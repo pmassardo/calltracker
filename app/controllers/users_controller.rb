f6a714a4fd54ff1462966e2ba9782bd80ac71d36
paul_massardo@hotmail.com <paul_massardo@hotmail.com>
2016-09-20 00:32:45 -0400

    modified user controller to skip require login for new and create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  skip_before_action :require_login, only: [:index, :new, :create]
+
+  skip_before_action :require_login, only: [:new, :create]
   # before_action :set_user, only: [:show, :edit, :update, :destroy]
   # skip_before_action :require_login, except: [:destroy]
 
