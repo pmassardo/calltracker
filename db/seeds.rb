d2b44cce720db4b03cf3f888128b8b31348a3afc
paul_massardo@hotmail.com <paul_massardo@hotmail.com>
2016-09-20 00:35:00 -0400

    added testing data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# Mayor.create(name: 'Emanuel', city: cities.first)
+
+Room.create(number: '102')
+Room.create(number: '104')
+Room.create(number: '106')
+Room.create(number: '202')
+Room.create(number: '204')
+Room.create(number: '206')
+Room.create(number: '302')
+Room.create(number: '304')
+Room.create(number: '306')
+
+Task.create(name: 'Ported')
+Task.create(name: 'Toileted')
+Task.create(name: 'Showered')
