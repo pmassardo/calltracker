8e676380230c9a762d649359430d3e97a48d6977
paul_massardo@hotmail.com <paul_massardo@hotmail.com>
2016-09-21 18:37:12 -0400

    add users/role to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
 
-  # get 'room_tasks/index'
-  #
-  # get 'room_tasks/new'
-  #
-  # get 'room_tasks/create'
-  #
-  # get 'room_tasks/edit'
-  #
-  # get 'room_tasks/update'
-  #
-  # get 'room_tasks/destroy'
-
   root :to => 'room_tasks#new'
 
   resources :room_tasks
@@ -19,6 +7,8 @@ Rails.application.routes.draw do
   post 'logout' => 'user_sessions#destroy', :as => :logout
 
   resources :users
+
+  patch '/users/', to: 'users#role'
 
   resources :user_sessions
 
