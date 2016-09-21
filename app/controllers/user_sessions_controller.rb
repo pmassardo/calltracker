6eb3eb601f33ff2b45ce0d17ffd63a6f735b6163
paul_massardo@hotmail.com <paul_massardo@hotmail.com>
2016-09-21 18:34:10 -0400

    changed alert to notice added spacing for readability

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,15 +3,22 @@ class UserSessionsController < ApplicationController
   skip_before_action :require_login, except: [:index, :destroy]
 
   def new
+
     @user = User.new
+
   end
 
   def create
+
     if @user = login(params[:email], params[:password])
+
       redirect_back_or_to(new_room_task_path, notice: 'Login successful')
+
     else
-      flash.now[:alert] = 'Login failed'
+
+      flash.now[:notice] = 'Login failed'
       render action: 'new'
+
     end
   end
 
